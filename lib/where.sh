4539d7c245314b0a1ae4a0bb92976f21e556d8dc
Jean BENOIT <Jean BENOIT jean@unistra.fr>
2015-12-15 11:46:58 +0100

    Bugfix: resolve IP address to name

--- lib/where.sh
+++ lib/where.sh
@@ -1,9 +1,18 @@
-getcomm() {
-  community=$(sed -n "/^$rname /s///p" $commfile)
 
+getcomm() {
+  rname=$(getname "$1")
+  community=$(sed -n "/^$rname /s///p" $commfile)
   if [ -z "$community" ] ; then
     community=$DEFAULT_COMMUNITY
   fi
 
   echo $community
+}
+
+getname() {
+  if echo $1 | egrep -q "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$" ; then
+    host $1|sed 's/.* //'|sed 's/\..*//'
+  else 
+    echo $1
+  fi
 }
